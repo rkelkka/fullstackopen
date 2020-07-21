a66767900e5987ac891bc67000b903598bc62c33
rkelkka <>
2020-07-21 15:01:13 +0300

    Part 1a, tasks  1.1 & 1.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,7 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
+	const Header = (props) => {
+		return (
+			<h1>{props.course}</h1>			
+		)
+	}
+	
+	const Part = (props) => {
+		return (
+			<p>{props.part} {props.exercisesCount}</p>			
+		)
+	}
+
+	const Content = (props) => {
+		let naivePartId = 1;
+		const renderPart = (part) => (
+			<Part key={naivePartId++} part={part.part} exercisesCount={part.exCount} />
+		)
+		return (
+			<div>
+				{props.parts.map(renderPart)}
+			</div>
+		)
+	}
+
+	const Total = (props) => {
+		return (
+			<p>Number of exercises {props.count}</p>
+		)
+	}
+
+	const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -11,19 +41,11 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+    <>
+      <Header course={course} />
+			<Content parts={[{part: part1, exCount: exercises1}, {part: part2, exCount: exercises2}, {part: part3, exCount: exercises3}]} />
+      <Total count = {exercises1 + exercises2 + exercises3} />
+    </>
   )
 }
 
